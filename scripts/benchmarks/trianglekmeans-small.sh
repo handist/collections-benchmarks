92ebf9556cd15b9a91a15b8f1fa761e7cb84a605
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-04-22 14:34:40 +0900

    Cleanup in benchmark directory

---
+++ scripts/benchmarks/trianglekmeans-small.sh
@@ -0,0 +1,13 @@
+# Configuration for the K-Means benchmark
+PROGRAM_LAUNCHER="${BENCH_HOME}/launchers/${HOST_TYPE}/launcher.sh"
+export BENCHMARK_NAME="kmeanstriangle"
+export MAIN="handist.kmeans.KMeansTriangleDistribution"
+# There are 5 compulsory arguments and 1 optional:
+# <point dimension> <nb of clusters "k"> <repetitions> <chunk size> <number of points> [seed]
+export ARGS="3 10 20 1000 10000000 42"
+export TIMEOUT=5m
+export REPETITIONS=1
+
+# Script used to combine the results of this benchmark
+PARSER=kmeans-parser.sh
+
