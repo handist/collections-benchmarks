2dc301ce550a92d9141c0481c8fd47e16f4aff2a
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-04-26 11:14:18 +0900

    Added various grain executions of triangle kmenas benchamrk

--- scripts/benchmarks/trianglekmeans2-small.sh
+++ scripts/benchmarks/trianglekmeans2-small.sh
@@ -1,10 +1,10 @@
 # Configuration for the K-Means benchmark
 PROGRAM_LAUNCHER="${BENCH_HOME}/launchers/${HOST_TYPE}/launcher-oneparam.sh"
-export BENCHMARK_NAME="kmeanstriangle"
+export BENCHMARK_NAME="kmeanstriangle2"
 export MAIN="handist.kmeans.KMeansTriangleDistribution"
 # There are 5 compulsory arguments and 1 optional:
 # <point dimension> <nb of clusters "k"> <repetitions> <chunk size> <number of points> [seed]
-export ARGS="5 10 20 10000 10000000 42"
+export ARGS="5 10 20 100 10000000 42"
 export TIMEOUT=10:00
 export REPETITIONS=1
 
@@ -12,4 +12,4 @@ export REPETITIONS=1
 PARSER=kmeans-parser.sh
 
 PARAM="GRAIN"
-VALUES="4 10 20 40 100 200 1000 10000"
+VALUES="4 10 20 40 100" # 200 1000 10000"
