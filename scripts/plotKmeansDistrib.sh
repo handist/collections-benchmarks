74daa0f466bf8b8c8798ca35136c70e74d78c482
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-05-11 17:28:08 +0900

    Added plot results from the 'huge' kmeans implementation

---
+++ scripts/plotKmeansDistrib.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+CONFIGS="NoLifeline-nolongpacking Hypercube-nolongpacking lifelinenoanswerafterintraLB"
+HOSTS="4"
+#PROBLEM="ufs-small ufs-large ufs-sfbt8 ufs-sfbt9 ufs-sfbt10 ufs-sfbt11 ufs-sfbt11v2 ufs2-small ufs2-large ufs2ub-small ufs2ub-large"
+PROBLEM="hugekmeanstriangle hugekmeansflat"
+
+echo "In directory `pwd`"
+
+# Make the directory in which all plots will be placed
+
+OUTDIR=kmeansplot
+
+mkdir -p ${OUTDIR}
+
+for cfg in $CONFIGS
+do
+    for hst in $HOSTS
+    do
+        for pb in $PROBLEM
+        do
+
+        FILES_TO_PARSE=`ls ofp${hst}/${cfg}/${pb}/*.out.txt`
+        for file in ${FILES_TO_PARSE}
+        do
+            PREFIX=${file/out.txt/png}
+            SUFFIX=${PREFIX#ofp${hst}/${cfg}/${pb}/}
+            OUTFILE="${OUTDIR}/ofp${hst}_${SUFFIX}"
+
+            if [[ -f ${OUTFILE} ]]
+            then
+                echo "Overwriting ${OUTFILE}"
+            else
+                echo "Creating file ${OUTFILE}"
+            fi
+            cat ${file} | sed '/^Starting\b/d' | sed '/Init;/d' | sed -s 's/$/;/' > data.csv
+            gnuplot -c gnuplot/kmeans-distribution.plt ${hst} data.csv > $OUTFILE
+
+        done
+        done
+    done
+done
+
+# Remove temporary file
+rm -f data.csv
