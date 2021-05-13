82c15896dc590604477f6f1f3877e6f0180f5c98
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-05-13 22:24:45 +0900

    Added concatenated scripts for detailed analyis

---
+++ scripts/results/csvforexcel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+HOST="ofp4"
+PROGRAMS="hugekmeansflat hugekmeanstriangle"
+CONFIGS="NoLifeline-nolongpacking Hypercube-nolongpacking lifelinenoanswerafterintraLB"
+
+for prog in ${PROGRAMS}
+do
+	# Produce a dedicated CSV for each program which concatenates the result of
+	# every config chosen
+	OUT=FOR_EXCEL_${prog}.csv
+	
+	echo "Config; Grain; Iteration; Elapsed Time(ms);" > ${OUT}
+
+	for cfg in ${CONFIGS}
+	do
+		FILE=${HOST}_${cfg}_${prog}.csv
+		cat ${FILE} | grep ";Iter "| sed "s/^/${cfg};/" >> ${OUT}
+	done
+done
