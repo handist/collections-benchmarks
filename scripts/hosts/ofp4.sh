b6721602e5a3950448609a9170fb7a6f3390d10a
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-07-28 12:40:21 +0900

    Added confifguration for new glb version with logger
    [skip ci]

--- scripts/hosts/ofp4.sh
+++ scripts/hosts/ofp4.sh
@@ -8,12 +8,14 @@ export HOST_TYPE=ofp
 #export BENCHMARKS="cardgame2-larger.sh cardgame2-huge.sh cardgame2-largerv2.sh cargame2-hugev2.sh flatcardgame-larger.sh flatcardgame-huge.sh"
 #export BENCHMARKS="ufs-small.sh ufs-sfbt8.sh ufs-sfbt9.sh ufs-sfbt10.sh ufs-sfbt11.sh ufs-sfbt11v2.sh ufs2-small.sh ufs2ub-small.sh ufs2-small-grain.sh"
 #export BENCHMARKS="kmeans-grain.sh trianglekmeans-small.sh flatkmeans-small.sh trianglekmeans-larger.sh flatkmeans-larger.sh flatkmeans-huge.sh trianglekmeans-huge.sh JFRkmeans-huge.sh"
-export BENCHMARKS="flatkmeans-huge.sh trianglekmeans-huge.sh JFRkmeans-huge.sh"
+export BENCHMARKS="flatkmeans-huge.sh trianglekmeans-huge.sh" # flatkmeans-huge-log.sh trianglekmeans-huge-log.sh"
 # trianglekmeans-larger.sh flatkmeans-larger.sh flatkmeans-huge.sh trianglekmeans-huge.sh"
 #"trianglekmeans2-small.sh"
 
 # List of library configruations that we want to test
-export CONFIGS="nolifeline-nolongboxing.sh hypercube-nolongboxing.sh lifeline-noanswerafterintraLB.sh hypercube-bugfix.sh" #" # lifeline-debug.sh
+#export CONFIGS="nolifeline-nolongboxing.sh hypercube-nolongboxing.sh lifeline-noanswerafterintraLB.sh
+export CONFIGS="logger-lifeline.sh logger-nolifeline.sh"
+#hypercube-bugfix.sh" #" # lifeline-debug.sh
 # Path to the MPI bindings
 export JAVALIBRARYPATH=/work/gp43/share/mpj-v0_44/lib
 # Number of hosts desired
