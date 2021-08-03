f724ed66dcc59b7d8ca311207b4ceca741ff366b
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-08-03 22:21:26 +0900

    Fixed main class for benchamrk script

--- scripts/benchmarks/fcm-flat-huge.sh
+++ scripts/benchmarks/fcm-flat-huge.sh
@@ -1,7 +1,7 @@
 # Configuration for the K-Means benchmark
 PROGRAM_LAUNCHER="${BENCH_HOME}/launchers/${HOST_TYPE}/launcher-oneparam.sh"
 export BENCHMARK_NAME="fcm-huge"
-export MAIN="handist.kmeans.KMeansFlatDistribution"
+export MAIN="handist.noglb.fuzzykmeans.FuzzyKMeans"
 # There are 6 compulsory arguments and 1 optional:
 # <point dimension> <nb of clusters "k"> <fizziness m> <repetitions> <chunk size> <number of points> [seed]
 export ARGS="5 10000 2.0 20 10000 10000000 42"
