52ecb02674ac766fe08efe76cb70eb0c936a8a7a
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-08-03 21:49:46 +0900

    Added scripts for OFP execution of FCM

---
+++ scripts/benchmarks/fcm-flat-huge.sh
@@ -0,0 +1,15 @@
+# Configuration for the K-Means benchmark
+PROGRAM_LAUNCHER="${BENCH_HOME}/launchers/${HOST_TYPE}/launcher-oneparam.sh"
+export BENCHMARK_NAME="fcm-huge"
+export MAIN="handist.kmeans.KMeansFlatDistribution"
+# There are 6 compulsory arguments and 1 optional:
+# <point dimension> <nb of clusters "k"> <fizziness m> <repetitions> <chunk size> <number of points> [seed]
+export ARGS="5 10000 2.0 20 10000 10000000 42"
+export TIMEOUT=10:00
+export REPETITIONS=1
+
+# Script used to combine the results of this benchmark
+PARSER=kmeans-grain-parser.sh
+
+PARAM="GRAIN"
+VALUES="4 10 20 40 100 200 400 1000 2000"
