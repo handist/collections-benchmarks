1ad493e90a8a02449adf3e71f0633fb9612af3e5
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-08-17 23:56:56 +0900

    Modified Kmeans program for easier weak scaling

--- src/main/java/handist/noglb/kmeans/KMeans.java
+++ src/main/java/handist/noglb/kmeans/KMeans.java
@@ -8,7 +8,6 @@ import java.util.stream.IntStream;
 
 import handist.collections.Chunk;
 import handist.collections.LongRange;
-import handist.collections.dist.CollectiveMoveManager;
 import handist.collections.dist.DistChunkedList;
 import handist.collections.dist.TeamedPlaceGroup;
 
@@ -59,21 +58,20 @@ public class KMeans {
     }
 
     public static void main(String[] args) {
-        // ARGUMENT PARSING
-        if (args.length < 5) {
-            printUsage();
-            return;
-        }
-
-        int dimension, k, repetitions, chunkSize, chunkCount, dataSize;
+        int dimension, k, repetitions, dataSize, chunkSize, chunkCount;
         long seed;
         try {
             dimension = Integer.parseInt(args[0]);
             k = Integer.parseInt(args[1]);
             repetitions = Integer.parseInt(args[2]);
-            chunkSize = Integer.parseInt(args[3]);
-            chunkCount = Integer.parseInt(args[4]);
-            dataSize = chunkSize * chunkCount;
+            dataSize = Integer.parseInt(args[3]);
+            chunkSize = Integer.parseInt(args[4]);
+            chunkCount = dataSize / chunkSize;
+
+            if (chunkCount * chunkSize != dataSize) {
+                System.err.println("Chunk size not uniform!");
+                return;
+            }
 
             if (args.length > 5) {
                 seed = Long.parseLong(args[5]);
@@ -86,7 +84,8 @@ public class KMeans {
             return;
         }
 
-        System.err.println(KMeans.class.getCanonicalName() + "; arguments received were " + Arrays.toString(args));
+        System.err.println(
+                SingleHostKMeans.class.getCanonicalName() + "; arguments received were " + Arrays.toString(args));
 
         // initialize final constants for easier lambda serialization later
         final int K = k;
@@ -95,40 +94,29 @@ public class KMeans {
 
         // INITIALIZATION
         final long initStart = System.nanoTime();
-        final Random r = new Random(seed);
+        final TeamedPlaceGroup world = TeamedPlaceGroup.getWorld();
 
         final DistChunkedList<Point> points = new DistChunkedList<>();
-        final List<Double[]> initialPoints = generateData(dataSize, dimension, k);
-        final List<Double[]> initialCentroids = randomSample(k, initialPoints, r);
-
-        // Additional step for our implementation, we need to place the points in our
-        // DistCol collection
-        for (int chunkNumber = 0; chunkNumber < chunkCount; chunkNumber++) {
-            final LongRange chunkRange = new LongRange(chunkNumber * chunkSize, (chunkNumber + 1) * chunkSize);
-            final Chunk<Point> c = new Chunk<>(chunkRange, l -> {
-                return new Point(initialPoints.get(l.intValue()));
-            });
-            points.add(c);
-        }
-
-        // Second additional step: we distribute the chunks evenly across hosts
-        final TeamedPlaceGroup world = TeamedPlaceGroup.getWorld();
         world.broadcastFlat(() -> {
-            final CollectiveMoveManager mm = new CollectiveMoveManager(world);
-            final int hostCount = world.size();
-            int destinationHost = 0;
-            for (final LongRange lr : points.ranges()) {
-                points.moveRangeAtSync(lr, world.get((destinationHost++) % hostCount), mm);
+            final List<Double[]> initialPoints = generateData(dataSize, dimension, k);
+            for (int chunkNumber = 0; chunkNumber < chunkCount; chunkNumber++) {
+                final LongRange chunkRange = new LongRange(chunkNumber * chunkSize, (chunkNumber + 1) * chunkSize);
+                final Chunk<Point> c = new Chunk<>(chunkRange, l -> {
+                    return new Point(initialPoints.get(l.intValue()));
+                });
+                points.add(c);
             }
-            mm.sync();
         });
 
-        // Third additional step, we convert the list of initial centroids to a 2Darray
+        final Random r = new Random(seed);
+        final List<Point> initialCentroids = randomSample(k, points, r);
+
+        // Second additional step, we convert the list of initial centroids to a 2Darray
         // of double
         final double[][] initialClusterCenter = new double[k][dimension];
         for (int i = 0; i < k; i++) {
             for (int j = 0; j < dimension; j++) {
-                initialClusterCenter[i][j] = initialCentroids.get(i)[j];
+                initialClusterCenter[i][j] = initialCentroids.get(i).position[j];
             }
         }
 
@@ -157,9 +145,11 @@ public class KMeans {
                 clusterCentroids = closestPoint.closestPointCoordinates;
 
                 final long iterEnd = System.nanoTime();
-                System.out.println("Iter " + iter + "; " + (iterEnd - iterStart) / 1e6 + "; " + "; "
-                        + (assignFinished - iterStart) / 1e6 + "; " + (avgFinished - assignFinished) / 1e6 + "; "
-                        + (iterEnd - avgFinished) / 1e6);
+                if (world.rank() == 0) {
+                    System.out.println("Iter " + iter + "; " + (iterEnd - iterStart) / 1e6 + "; " + "; "
+                            + (assignFinished - iterStart) / 1e6 + "; " + (avgFinished - assignFinished) / 1e6 + "; "
+                            + (iterEnd - avgFinished) / 1e6);
+                }
             }
         });
     }
@@ -169,7 +159,7 @@ public class KMeans {
      */
     private static void printUsage() {
         System.err.println("Usage: java -cp [...] " + KMeans.class.getCanonicalName()
-                + " <point dimension> <nb of clusters \"k\"> <repetitions> <chunk size> <number of chunks> [<seed>]");
+                + " <point dimension> <nb of clusters \"k\"> <repetitions> <nb points/host> <chunk size> [<seed>]");
     }
 
     /**
@@ -180,8 +170,8 @@ public class KMeans {
      * @param random      random generator
      * @return a list of initial centroids
      */
-    static List<Double[]> randomSample(final int sampleCount, final List<Double[]> data, final Random random) {
-        return random.ints(sampleCount, 0, data.size()).mapToObj(data::get).collect(Collectors.toList());
+    static List<Point> randomSample(final int sampleCount, DistChunkedList<Point> data, final Random random) {
+        return random.ints(sampleCount, 0, (int) data.size()).mapToObj(data::get).collect(Collectors.toList());
     }
 
     static double weightedAverage(double a, long includedPoints, double b, long includedPoints2) {
