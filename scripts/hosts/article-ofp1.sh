97cf1fe1b2e2a4363767f4e3f8193cf648411979
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-08-17 18:09:46 +0900

    fixed config for 1 host ofp

--- scripts/hosts/article-ofp1.sh
+++ scripts/hosts/article-ofp1.sh
@@ -7,7 +7,7 @@ export HOST_TYPE=ofp
 # List of benchmarks that we want to run
 export BENCHMARKS=""
 # List of library configruations that we want to test
-export CONFIGS="nolifeline-nolongboxing.sh"
+export CONFIGS="logger-nolifeline.sh"
 # Path to the MPI bindings
 export JAVALIBRARYPATH=/work/gp43/share/mpj-v0_44/lib
 # Number of hosts desired
