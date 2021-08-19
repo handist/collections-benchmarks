d9fce3967b2f322e81ddee2312b979c94484207d
kawanishi <kawanishi@fine.cs.kobe-u.ac.jp>
2021-08-20 03:04:17 +0900

    Added a 76branch collections jar file and a configuration file for it

---
+++ scripts/configurations/76-2dtrial-outputproduct.sh
@@ -0,0 +1,7 @@
+################################################################################
+# Configuration for the collections library whose last commit was the merge    #
+# from branch 78                                                               #
+################################################################################
+export CONFIG_NAME=""
+export COLLECTIONS_LIBRARY=${BENCH_HOME}/jar/collections-76trial2d.jar
+export DEPENDENCIES=${BENCH_HOME}/jar/deps
