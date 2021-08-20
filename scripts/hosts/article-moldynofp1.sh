8c0bbaa1c10dbbc9af985ebee0922e99185b0d5a
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-08-20 17:42:28 +0900

    Execution on OFP of first moldyn version

---
+++ scripts/hosts/article-moldynofp1.sh
@@ -0,0 +1,22 @@
+################################################################################
+# Configuration for executions on OFP                                          #
+################################################################################
+export HOST=article-moldynofp1
+export HOST_TYPE=ofp
+
+# List of benchmarks that we want to run
+export BENCHMARKS="moldyn.sh"
+# List of library configruations that we want to test
+export CONFIGS="76-2dtrial-outputproduct.sh"
+# Path to the MPI bindings
+export JAVALIBRARYPATH=/work/gp43/share/mpj-v0_44/lib
+# Number of hosts desired
+export NB_HOSTS=1
+# Number of concurrent workers *(-Dglb.workers)
+export WORKERS=68
+# Core restriction
+export CORE_RESTRICTION="0-67"
+
+########################## Options specific to OFP #############################
+# Resource group
+export RSCGRP="regular-flat"
---
+++ scripts/launchers/ofp/directmpiprogram.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PJM -g gp43
+
+export MPJ_HOME=/work/gp43/share/mpj-v0_44
+export I_MPI_PIN=0
+
+mpiexec.hydra -n ${PJM_MPI_PROC} \
+taskset -ca ${CORE_RESTRICTION} \
+java -cp ${COLLECTIONS_LIBRARY}:${DEPENDENCIES}/* \
+-Xms40g -Xmx80g \
+-Djava.library.path=${JAVALIBRARYPATH} \
+-Dglb.grain=${GRAIN} \
+-Dglb.lifeline=${LIFELINE} \
+-Dglb.serialization=${SERIALIZATION} \
+-Dglb.trace=${TRACE} \
+${MAIN} 0 0 native ${ARGS} ${EXTRA_ARGS}
