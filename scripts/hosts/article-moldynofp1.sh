34e009804d8ae48136b514cf64352ed378990cb3
p43002 <p43002@ofp04.ofp>
2021-08-22 06:24:39 +0900

    Runned moldyn at ofp

--- scripts/hosts/article-moldynofp1.sh
+++ scripts/hosts/article-moldynofp1.sh
@@ -5,7 +5,7 @@ export HOST=article-moldynofp1
 export HOST_TYPE=ofp
 
 # List of benchmarks that we want to run
-export BENCHMARKS=("moldyn-hybrid.sh")
+export BENCHMARKS="moldyn-hybrid.sh moldyn-seqmt.sh moldyn-seq.sh moldyn-mpi.sh moldyn-simpleseq.sh"
 # List of library configruations that we want to test
 export CONFIGS="76-2dtrial-outputproduct.sh"
 # Path to the MPI bindings
