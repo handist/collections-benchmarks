ef239ecc7a574dc6456e49866ec1a403e13d3e2e
p43002 <p43002@ofp04.ofp>
2021-08-23 07:44:11 +0900

    Run moldyn dist

--- scripts/hosts/article-moldynofp1.sh
+++ scripts/hosts/article-moldynofp1.sh
@@ -5,7 +5,7 @@ export HOST=article-moldynofp1
 export HOST_TYPE=ofp
 
 # List of benchmarks that we want to run
-export BENCHMARKS="moldyn-hybrid.sh moldyn-seqmt.sh moldyn-seq.sh moldyn-mpi.sh moldyn-simpleseq.sh"
+export BENCHMARKS="moldyn-hybrid.sh moldyn-seqmt.sh moldyn-seq.sh moldyn-mpi.sh moldyn-simpleseq.sh moldyn-dist.sh"
 # List of library configruations that we want to test
 export CONFIGS="76-2dtrial-outputproduct.sh"
 # Path to the MPI bindings
--- scripts/hosts/article-moldynofp16.sh
+++ scripts/hosts/article-moldynofp16.sh
@@ -5,7 +5,7 @@ export HOST=article-moldynofp16
 export HOST_TYPE=ofp
 
 # List of benchmarks that we want to run
-export BENCHMARKS="moldyn-hybrid.sh moldyn-mpi.sh"
+export BENCHMARKS="moldyn-hybrid.sh moldyn-mpi.sh moldyn-dist.sh"
 # List of library configruations that we want to test
 export CONFIGS="76-2dtrial-outputproduct.sh"
 # Path to the MPI bindings
