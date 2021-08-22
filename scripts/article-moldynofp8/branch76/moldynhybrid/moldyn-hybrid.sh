c7888c84f12b33ea0e9a1a4bd6e26b9f5512ebe0
p43002 <p43002@ofp06.ofp>
2021-08-22 23:41:27 +0900

    Run moldyn n hosts

---
+++ scripts/article-moldynofp8/branch76/moldynhybrid/moldyn-hybrid.sh
@@ -0,0 +1,13 @@
+# Configuration for the Moldyn benchmark
+PROGRAM_LAUNCHER="${BENCH_HOME}/launchers/${HOST_TYPE}/launcher.sh"
+export BENCHMARK_NAME="moldynhybrid"
+export MAIN="handist.moldyn.MoldynHybrid"
+# There are 4 compulsory arguments and 1 optional:
+# <datasize index(0or1)> <number of workers> <number of split>
+export ARGS="1 ${WORKERS} 32 ${HOST}_hybrid_${WORKERS}.csv"
+export TIMEOUT=5:00
+export BEO_TIMEOUT=5m
+export REPETITIONS=5
+
+# Script used to combine the results of this benchmark
+#PARSER=kmeans-parser.sh
