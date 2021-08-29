f58e8cf0ce59f813be31a38692bc1bdac3ec0ee5
Patrick Finnerty <finnerty.patrick@fine.cs.kobe-u.ac.jp>
2021-08-29 12:18:03 +0900

    Added scripts and results of bag and concurrent list comparision

--- scripts/hosts/article-ofp1.sh
+++ scripts/hosts/article-ofp1.sh
@@ -6,7 +6,7 @@ export HOST_TYPE=ofp
 
 # List of benchmarks that we want to run
 #export BENCHMARKS=""
-export BENCHMARKS="article-ofp-renaissancekmeans.sh article-ofp-nompikmeans.sh article-ofp-teamedkmeans.sh article-ofp-teamedkmeans2.sh article-ofp-nompikmeans2.sh article-ofp-renaissancekmeans2.sh article-ofp-renaissancekmeans3.sh article-ofp-nompikmeans3.sh article-ofp-teamedkmeans3.sh"
+export BENCHMARKS="article-ofp-renaissancekmeans.sh article-ofp-nompikmeans.sh article-ofp-teamedkmeans.sh article-ofp-teamedkmeans2.sh article-ofp-nompikmeans2.sh article-ofp-renaissancekmeans2.sh article-ofp-renaissancekmeans3.sh article-ofp-nompikmeans3.sh article-ofp-teamedkmeans3.sh article-receiver-bag.sh article-receiver-syncal.sh article-receiver-conclist.sh article-receiver-bag4t.sh article-receiver-syncal4t.sh article-receiver-conclist4t.sh"
 # List of library configruations that we want to test
 export CONFIGS="logger-nolifeline.sh"
 # Path to the MPI bindings
